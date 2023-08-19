050df86a7294260ad64b7e07b22e3ad747fee5f4
Shirley-Buitrago <shirleybuitrago@gmail.com>
2023-08-18 23:42:21 -0500

    Create Ejercicio2.java

---
+++ Ejercicio2.java
@@ -0,0 +1,12 @@
+public class TrabajoenclasesB1 {
+    public static void main(String[] args) {
+
+        int anonacimiento = 1976;
+        int anoactual = 2023;
+
+        int edad= anoactual - anonacimiento;
+        System.out.println("Su edad es: " + edad + " años.");
+
+
+    }
+}
